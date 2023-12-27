4586331cfec9ece7b91d0e1e138fe7bf72d3b205
EricMarinMejia <ericmarin14@hotmail.com>
2023-12-26 22:49:05 -0500

    Ajout des paramètres de la base de données

---
+++ SchoolApi/Models/SchoolDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApi.Models;
+
+public class SchoolDatabaseSettings
+{
+
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string StudentsCollectionName { get; set; } = null!;
+    public string TeachersCollectionName { get; set; } = null!;
+    public string ClassesCollectionName { get; set; } = null!;
+
+}
